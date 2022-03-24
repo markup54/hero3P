b41ca0c6c1d34302a281a9b868997c9a82507d03
markup54 <marzenakupka@tlen.pl>
2022-03-24 13:56:39 +0100

    lista

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,8 @@
+import {Hero} from './hero';
+export const HEROES: Hero[] =[
+    {id:11, name:"Piorun"},
+    {id:12, name:"Pajączek"},
+    {id:13, name:"Magma"},
+    {id:14, name:"Nietoperek"},
+    {id:15, name:"Mrożon"}
+];
